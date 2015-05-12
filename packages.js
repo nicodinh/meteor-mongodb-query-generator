e8c70308da314ad586aa948fa2cb947b600009f0
AZARI Nicolas <nicolas@azari.fr>
2015-05-12 13:22:46 +0200

    add packages file

---
+++ packages.js
@@ -0,0 +1,11 @@
+Package.describe({
+    name: 'nicodinh:mongodb-query-generator',
+    summary: 'MongoDB Query Generator is a Meteor package',
+    version: '0.1',
+    git: 'https://github.com/nicodinh/meteor-mongodb-query-generator.git'
+});
+
+Package.onUse(function(api) {
+    api.addFiles([
+    ], 'client');
+});
